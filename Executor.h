e60ff2c60ed633feddc7931cb58512098fd39619
JagYui <JagYui@gmail.com>
2012-02-12 10:59:48 +0000

    Nothing yet, this is my next area to work on.

---
+++ Executor.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Executor.h
+ * Author: Jag
+ *
+ * Created on February 11, 2012, 11:38 AM
+ */
+
+#ifndef EXECUTOR_H
+#define	EXECUTOR_H
+
+class Executor {
+public:
+    Executor();
+    virtual ~Executor();
+    
+private:
+
+};
+
+#endif	/* EXECUTOR_H */
