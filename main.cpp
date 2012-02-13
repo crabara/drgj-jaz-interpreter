17daf62badbfa7a5343c5dae6cb847c92c9d9857
JagYui <JagYui@gmail.com>
2012-02-13 14:58:50 +0000

    Just cleaning up and uncommenting some lines so the executer actually runs.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
-/* 
- * File:   main.cpp
- * Author: Jag
- *
- * Created on February 11, 2012, 5:42 AM
- */
-
 #include "Reader.h"
 #include "Executer.h"
 
 int main(int argc, char** argv) {
-    //Just testing code right now.  This shows me that the linked list and
-    //reader functions are working correctly.
     Reader readFile;
     Executer runInterpreter;
     string fileName("");
     string outFileName("");
     ifstream is;
     ofstream os;
-    
     if (argc > 1) {
         fileName = argv[1];
         if (!fileName.compare(fileName.length() - 4, 4, ".jaz")) {
@@ -37,25 +27,13 @@ int main(int argc, char** argv) {
         getchar();
         return EXIT_FAILURE;
     }
-    
     is.open(fileName.c_str());
     if (is.is_open()) {
         runInterpreter.setInstructionList(readFile.buildList(is));
         is.close();
-        
-        //InstructionItem* listHead = runInterpreter.getCurrentInstruction();
-        //while (listHead->getNext() != NULL) {
-        //    cout << listHead->getLineNumber() << " " << listHead->getCommand() << " " << listHead->getArg() << endl;
-        //    listHead = listHead->getNext();
-        //}
-        //cout << listHead->getLineNumber() << " " << listHead->getCommand() << " " << listHead->getArg() << endl << endl;
-        
         runInterpreter.setLabelMap(
             readFile.mapLabels(
                 runInterpreter.getCurrentInstruction()));
-        
-        //map<string, InstructionItem*> testMap = runInterpreter.getLabelMap();
-        //cout << testMap["factorial"] << endl;
     } else {
         cout << "ERR: Cannot open file: " << fileName << endl
              << "JAZ: Halting execution.\n\n"
@@ -63,23 +41,18 @@ int main(int argc, char** argv) {
             getchar();
             return EXIT_FAILURE;
     }
-    
     os.open(outFileName.c_str());
     if (os.is_open()) {
         runInterpreter.execute(os);
-        //os << "test" << endl;
         os.close();
         cout << "JAZ: Output written to file: " << outFileName
              << endl << endl;
     } else {
         cout << "ERR: Output file could not be opened: " << outFileName << endl;
         cout << "JAZ: Output will only be displayed to the command line.\n\n";
-        //runInterpreter.execute(os, false);
+        runInterpreter.execute(os, false);
     }
-    
     cout << "Press ENTER to exit.";
     getchar();
-
     return 0;
 }
-
