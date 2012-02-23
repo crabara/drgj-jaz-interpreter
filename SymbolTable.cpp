46a7fc42726fd3a74dff00fb90d09878ce54977a
JagYui <JagYui@gmail.com>
2012-02-23 06:25:08 +0000

    b

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,15 +1,16 @@
 #include "SymbolTable.h"
 
-SymbolTable::SymbolTable() {
-}
+SymbolTable::SymbolTable() { }
 
-SymbolTable::~SymbolTable() {
-}
+SymbolTable::~SymbolTable() { }
 
+//sets a variable into the table by creating or overwriting a key (lvalue) with
+//a value (rvalue).
 void SymbolTable::setVariable(string key, string value) {
     this->variableMap[key] = value;
 }
 
+//resolves a rvalue by using its lvalue as a key to the map.
 string SymbolTable::getVariable(string key) {
     return this->variableMap[key];
 }
