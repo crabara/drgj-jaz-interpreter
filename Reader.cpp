afee3d3bee790e67aa60e32ff11869481d203262
JagYui@gmail.com <JagYui@gmail.com@14ad289b-e410-b8a6-a779-278b03bc37f3>
2012-02-23 06:25:49 +0000

    s

--- Reader.cpp
+++ Reader.cpp
@@ -7,76 +7,90 @@ Reader::Reader() {
 Reader::~Reader() {
 }
 
+//buildList recurses over each line of the jaz file until it reaches the end.
+//It then collapses backwards, returning pointers to each item in the list
+//until it returns a pointer to the head of the list to whatever called it in
+//the first place.
 InstructionItem* Reader::buildList(ifstream& is) {
+    //update the line number.
     lineTracker++;
     InstructionItem* listItem = NULL;
+    
+    //pull in a line from the jaz file and convert it into a string.
     char holder[100];
     is.getline(holder, 100);
     string line(holder);
+    
+    //parse the line into a command and an argument.
     pair<string, string> instrPair = parseLine(line);
+    
+    //check to see if the line was blank.
     if (instrPair.first.length() == 0) {
+        
         if (!is.eof()) {
+            //the line is blank, but there are more lines to parse.
             listItem = buildList(is);
         }
+        
     } else {
+        //the line is useful, put it at the end of the linked list.
         listItem = new InstructionItem(instrPair.first,
                                        instrPair.second,
                                        lineTracker);
+        
         if (!is.eof()) {
+            //there are more lines to parse, continue on.
             listItem->setNext(buildList(is));
         }
     }
     return listItem;
 }
 
-map<string, InstructionItem*> Reader::mapLabels(InstructionItem* listItem) {
-    map<string, InstructionItem*> labelList;
-    while (listItem->getNext() != NULL) {
-        if (listItem->getCommand() == "label") {
-            if (listItem->getArg().length() > 0) {
-                labelList[listItem->getArg()] = listItem->getNext();
-            } else {
-                cout << "ERR: Line " << listItem->getLineNumber()
-                     << " - 'label' requires an argument.\n"
-                     << "JAZ: This label will be ignored.\n\n";
-            }
-        }
-        listItem = listItem->getNext();
-    }
-    if (listItem->getCommand() == "label") {
-        if (listItem->getCommand() == "label") {
-            if (listItem->getArg().length() > 0) {
-                labelList[listItem->getArg()] = listItem->getNext();
-                cout << "ERR: Line " << listItem->getLineNumber()
-                     << " - 'label " << listItem->getArg()
-                     << "' is placed at the last line of the file.\n"
-                     << "JAZ: Any jumps to this label will be ignored.\n\n";
-            } else {
-                cout << "ERR: Line " << listItem->getLineNumber()
-                     << "-  'label' requires an argument.\n"
-                     << "JAZ: This label will be ignored.\n\n";
-            }
-        }
-    }
-    return labelList;
-}
-
+//this method simply parses a line into two parts, the command and the argument.
 pair<string, string> Reader::parseLine(string line) {
     pair<string, string> out = make_pair("", "");
+    
+    //these are the characters that should be ignored, space, tab and return.
     string parseSymbols(" \t\r");
+    
+    //these size_t's are used to track and remove superfluous white space from
+    //the line.
     size_t commandBegin = 0,
            commandEnd = 0,
            argBegin = 0,
            argEnd = 0;
+    
+    //find the first character in the line that is not white space.
     commandBegin = line.find_first_not_of(parseSymbols);
+    
+    //check to see if the line is blank.
     if (commandBegin != string::npos) {
+        //find the end of the command.
         commandEnd = line.find_first_of(parseSymbols, commandBegin);
+        //copy the command to the first part of the pair.
         out.first = line.substr(commandBegin, commandEnd - commandBegin);
-        argBegin = line.find_first_not_of(parseSymbols, commandEnd);
-        if (argBegin != string::npos) {
-            argEnd = line.find_last_not_of(parseSymbols);
-            out.second = line.substr(argBegin, (argEnd - argBegin) + 1);
+        
+        //check to see if this line is a show command (a special case).
+        if (out.first == "show") {
+            //show means the entirety of the rest of the line is the argument.
+            //This includes all the whitespace.
+            argBegin = commandEnd + 1;
+            out.second = line.substr(argBegin);
+            
+        } else {
+            //find the first character of the argument.
+            argBegin = line.find_first_not_of(parseSymbols, commandEnd);
+            
+            //test if this line does not have an argument.
+            if (argBegin != string::npos) {
+                //find the end of the command.
+                argEnd = line.find_first_of(parseSymbols, argBegin);
+                //copy the argument to the second part of the pair.
+                out.second = line.substr(argBegin, argEnd - argBegin);
+            }
         }
     }
+    
+    //send back the parsed line.
     return out;
 }
