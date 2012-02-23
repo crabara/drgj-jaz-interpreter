c8537f9cee5c93e3cf098193130532736ce6260b
JagYui <JagYui@gmail.com>
2012-02-23 06:25:28 +0000

    c

--- Reader.h
+++ Reader.h
@@ -2,19 +2,20 @@
 #define	READER_H
 
 #include "InstructionItem.h"
-#include <iostream>
 #include <fstream>
-#include <map>
 
+//Reader iterates through the jaz file line by line and builds a linked list of
+//instructions, returning a pointer to the first instruction to the caller.
 class Reader {
 public:
     Reader();
     virtual ~Reader();
     
     InstructionItem* buildList(ifstream& is);
-    map<string, InstructionItem*> mapLabels(InstructionItem* listItem);
     
 private:
+    //lineTracker increments each time buildList is called, it tracks what line
+    //the current instruction appears on in the jaz file.
     int lineTracker;
     
     pair<string, string> parseLine(string line);
