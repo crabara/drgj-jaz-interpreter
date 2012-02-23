ded78c0be2f69f6d4387a502c20757281c2d7cdb
JagYui@gmail.com <JagYui@gmail.com@14ad289b-e410-b8a6-a779-278b03bc37f3>
2012-02-23 06:26:16 +0000

    s

--- InstructionItem.h
+++ InstructionItem.h
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+//Instruction item is a simple linked list of objects.  Each object contains
+//the command and argument from a single line in a jaz file.  The other
+//variables are used for error control and reporting.
 class InstructionItem {
-    
 public:
     InstructionItem();
     InstructionItem(string tempCommand, string tempArg, int tempLineNumber);
@@ -25,10 +27,16 @@ public:
     InstructionItem* getNext();
     
 private:
+    //number of the line on which this instruction is written in the jaz file.
     int lineNumber;
+    //this holds how many times this line was accessed in order to provide more
+    //robust error tracking to the jaz file writer.
     int accessAttempt;
+    //the command of the given line.
     string command;
+    //the argument of a given line.
     string arg;
+    //a pointer to the next instruction.
     InstructionItem* next;
 
 };
